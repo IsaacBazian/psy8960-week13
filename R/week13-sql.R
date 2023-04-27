0791bf6a6dcbc9b0b9cd9001ebead5e58bd9572d
Isaac M Bazian <bazia001@umn.edu>
2023-04-27 14:56:49 -0500

    SQL analysis underway, about half done

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -19,5 +19,28 @@ conn <- dbConnect(MariaDB(),
 
 dbExecute(conn, "USE cla_tntlab;")
 
+# Analysis
+
+# Total number of managers
+dbGetQuery(conn, "SELECT COUNT(*) 
+           FROM datascience_8960_table;")
+
+# Total number of unique managers by id
+dbGetQuery(conn, "SELECT COUNT(DISTINCT employee_id) 
+           FROM datascience_8960_table;")
+
+# Summary of managers by location, not hired as managers
+dbGetQuery(conn, "SELECT city, COUNT(employee_id) 
+           FROM datascience_8960_table
+           WHERE manager_hire = 'N'
+           GROUP BY city;")
+
+
+
+
+
+
+
+
 
 
