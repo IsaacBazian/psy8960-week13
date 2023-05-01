b10e85499bf5d61441adcb9302fa9696e7b50197
Isaac M Bazian <bazia001@umn.edu>
2023-04-30 21:38:54 -0500

    Added comments to each pipe / request to explain how they work

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -29,14 +29,21 @@ week13_tbl <- read_csv("../data/week13.csv")
 # Analysis
 
 # Total number of managers
+# Every row of this table represents a manager, so this code simply gets the 
+# number of rows in the table
 n_managers <- nrow(week13_tbl)
 n_managers
 
 # Total number of unique managers by id
+# The unique function returns a vector with every unique value of employee_id;
+# therefore, the number of entries in that vector is the number of unique managers
 n_managers_unique <- length(unique(week13_tbl$employee_id))
 n_managers_unique
 
 # Summary of managers by location, not hired as managers
+# First we filter to only get the managers not originally hired as managers,
+# then we group the remainder by the city they work in, and get the count
+# of managers in each group to get managers per city
 managers_by_location_not_hired_tbl <- week13_tbl %>% 
   filter(manager_hire == "N") %>% 
   group_by(city) %>%
@@ -44,12 +51,22 @@ managers_by_location_not_hired_tbl <- week13_tbl %>%
 managers_by_location_not_hired_tbl
 
 # Average and standard deviation of tenure split by performance level
+# First we group managers by their performance level, then we calculate
+# the mean and standard deviation of number of years of employment for each group
 tenure_tbl <- week13_tbl %>% 
   group_by(performance_group) %>% 
   summarise(average_tenure = mean(yrs_employed), sd_tenure = sd(yrs_employed))
 tenure_tbl
 
 # Top 3 managers by location
+# First we group managers by city, then we arrange the table first in alphabetical
+# order of city, then by descending test_score. We then use mutate to add a rank
+# to each manager based on their descending test_score, filter to only include
+# those managers with a rank of 3 or less, and then select only the 3 variables
+# we want in the end. Note that by using dense_rank, there are no gaps in sequential
+# ranks even if there are ties; the instructions were ambiguous as to whether
+# these gaps are desired or not, so I have erred on the side of including 
+# more information.
 top_managers_tbl <- week13_tbl %>% 
   group_by(city) %>% 
   arrange(city, desc(test_score)) %>% 
--- R/week13-sql.R
+++ R/week13-sql.R
@@ -22,25 +22,39 @@ dbExecute(conn, "USE cla_tntlab;")
 # Analysis
 
 # Total number of managers
+# Every row of this table represents a manager, so this code simply gets the 
+# number of rows in the table
 dbGetQuery(conn, "SELECT COUNT(*) 
            FROM datascience_8960_table;")
 
 # Total number of unique managers by id
+# This code returns the count of how many distinct (that is, unique) employee_id
+# values there are in this table
 dbGetQuery(conn, "SELECT COUNT(DISTINCT employee_id) 
            FROM datascience_8960_table;")
 
 # Summary of managers by location, not hired as managers
+# This code keeps only managers who were not originally hired as managers, groups them
+# by city, and displays the count of managers in each city
 dbGetQuery(conn, "SELECT city, COUNT(employee_id) 
            FROM datascience_8960_table
            WHERE manager_hire = 'N'
            GROUP BY city;")
 
 # Average and standard deviation of tenure split by performance level
+# This code groups managers by performance_group, then calculates and displays 
+# the mean and standard deviation of number of years of employment by each group
 dbGetQuery(conn, "SELECT performance_group, AVG(yrs_employed), STDDEV(yrs_employed)
            FROM datascience_8960_table
            GROUP BY performance_group;")
 
 # Top 3 managers by location
+# This code creates a temporary field in the table, which is the dense ranks of
+# descending manager test scores within each city. It then selects the 3 variables
+# we want in the end wherever a manager's rank was less than or equal to 3.
+# Note that by using DENSE_RANK, there are no gaps in sequential
+# ranks even if there are ties; the instructions were ambiguous as to whether
+# these gaps are desired or not, so I have erred on the side of including 
 dbGetQuery(conn, "WITH temp_ranked AS (
            SELECT *, DENSE_RANK() OVER(PARTITION BY city ORDER BY test_score DESC) AS manager_rank
            FROM datascience_8960_table)
